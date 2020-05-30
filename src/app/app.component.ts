e98dcd8c2d402db0480ded7c7ef2a3757a3dd8a6
shyam <shyam@sumedhasoftech.com>
2020-05-30 10:45:03 +0530

    Remove non required depedency and property from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Synthelic';
-  constructor( private router: Router){
+export class AppComponent
+{  
+  constructor()
+  {
 
   }
-  
-  ngOnInit(){
-  
+
+  ngOnInit()
+  {
+
   }
 }
