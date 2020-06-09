a90462b2faba8ea526d2309d6f2217a50540fb0d
shyam <shyam@sumedhasoftech.com>
2020-06-09 11:48:23 +0530

    Show and hide modal dynamically

--- src/app/_modal/modal.component.ts
+++ src/app/_modal/modal.component.ts
@@ -12,8 +12,8 @@ export class ModalComponent implements OnInit, OnDestroy {
     @Input() id: string;
     private element: any;
 
-    constructor(private modalService: ModalService, private el: ElementRef) {
-        this.element = el.nativeElement;
+    constructor(private modalService: ModalService, private el: ElementRef) {        
+        this.element = el.nativeElement;        
     }
 
     ngOnInit(): void {
@@ -27,7 +27,7 @@ export class ModalComponent implements OnInit, OnDestroy {
         document.body.appendChild(this.element);
 
         // close modal on background click
-        this.element.addEventListener('click', el => {
+        this.element.addEventListener('click', el => {            
             if (el.target.className === 'jw-modal') {
                 this.close();
             }
@@ -51,6 +51,7 @@ export class ModalComponent implements OnInit, OnDestroy {
 
     // close modal
     close(): void {
+        console.log('Close call');
         this.element.style.display = 'none';
         document.body.classList.remove('jw-modal-open');
     }
